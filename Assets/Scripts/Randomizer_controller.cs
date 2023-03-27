407faad5c6b2f599c2816aeca2834ac9eb352b44
Nino Cauli <nigno17@gmail.com>
2023-03-27 11:11:38 +0200

    Adding new backgrounds

--- Assets/Scripts/Randomizer_controller.cs
+++ Assets/Scripts/Randomizer_controller.cs
@@ -11,32 +11,19 @@ namespace UnityEngine
         private GameObject dirLight;
         private Object[] modelList;
         private Object[] skyMaterialList;
-        // private Object[] floorMaterialList;
         private Object[] modelMaterialList;
         private int modelCounter;
         private int skyMaterialCounter;
-        // private int floorMaterialCounter;
         private int modelMaterialCounter;
         private RandCameraController mainCameraController;
-        // private MeshRenderer floorMeshRenderer;
         private string tempFolderName;
         private bool isRecording;
-        
-        // [SerializeField] GameObject mainMenu;
-        // [SerializeField] GameObject ingameMenu;
-        // [SerializeField] GameObject settingsMenu;
-        // [SerializeField] GameObject animLoaderMenu;
-        // [Space(10)]
+    
 
         [Header("Folder where the dataset will be saved")]
         [Space(5)]
         [SerializeField] string screensFolderName = "";
         [Space(10)]
-
-        // [Header("Add mirrored animations (flag)")]
-        // [Space(5)]
-        // [SerializeField] bool addMirrors = false;
-        // [Space(10)]
 
         [Header("Directional light")]
         [Space(5)]
@@ -66,22 +53,17 @@ namespace UnityEngine
             modelCounter = -1;
             skyMaterialCounter = -1;
             modelMaterialCounter = -1;
-            // floorMaterialCounter = -1;
             modelMaterialCounter = -1;
             tempFolderName = "";
 
             modelList = Resources.LoadAll("Models", typeof(GameObject));
             skyMaterialList = Resources.LoadAll("HDRIHaven/Materials", typeof(Material));
-            // floorMaterialList = Resources.LoadAll("floor/Materials", typeof(Material));
             modelMaterialList = Resources.LoadAll("Materials", typeof(Material));
 
             dirLight = GameObject.Find("Directional Light");
 
             GameObject temp_randomizer = GameObject.Find("Main Camera");
             mainCameraController = temp_randomizer.GetComponent<RandCameraController>();
-            
-            // GameObject temp_floor = GameObject.Find("Floor");
-            // floorMeshRenderer = temp_floor.GetComponent<MeshRenderer>();
 
             Debug.Log("Randomizer started...");
         }
@@ -101,8 +83,8 @@ namespace UnityEngine
         {
             Destroy(loadedModel);
             loadedModel = spawnNextModel();
-            MeshRenderer modelMeshRenderer = loadedModel.GetComponent<MeshRenderer>();
-            modelMeshRenderer.material = spawnNextModelMaterial();
+            // MeshRenderer modelMeshRenderer = loadedModel.GetComponent<MeshRenderer>();
+            // modelMeshRenderer.material = spawnNextModelMaterial();
 
             if (useSkyBoxRandomization)
             {
@@ -116,64 +98,18 @@ namespace UnityEngine
             if (isColorRandomized)
                 tempLight.color = Random.ColorHSV();
 
-            // floorMeshRenderer.material = spawnNextFloorMaterial();
 
             mainCameraController.setRandFlag(true);
         }
-
-        // public void startRecording()
-        // {
-        //     isRecording = true;
-        //     mainMenu.SetActive(false);
-        //     ingameMenu.SetActive(true);
-        //     settingsMenu.SetActive(false);
-        //     animLoaderMenu.SetActive(false);
-        // }
-
-        // public void goToMain()
-        // {
-        //     isRecording = false;
-        //     mainMenu.SetActive(true);
-        //     ingameMenu.SetActive(false);
-        //     settingsMenu.SetActive(false);
-        //     animLoaderMenu.SetActive(false);
-        // }
-
-        // public void goToSettings()
-        // {
-        //     isRecording = false;
-        //     mainMenu.SetActive(false);
-        //     ingameMenu.SetActive(false);
-        //     settingsMenu.SetActive(true);
-        //     animLoaderMenu.SetActive(false);
-        // }
-
-        // public void goToAnimLoader()
-        // {
-        //     isRecording = false;
-        //     mainMenu.SetActive(false);
-        //     ingameMenu.SetActive(false);
-        //     settingsMenu.SetActive(false);
-        //     animLoaderMenu.SetActive(true);
-        // }
 
         void saveOnDisk()
         {
              // Debug.Log(Directory.Exists("Assets"));
             string folderName = screensFolderName + "/" + modelList[modelCounter].name + "/";
-            //string folderName = "Screens/" + modelList[modelCounter].name + "/";
             if(Directory.Exists(folderName) == false)
             {
                 Directory.CreateDirectory(folderName);
             }
-
-            // int dirCounter = 0;
-            // tempFolderName = folderName + dirCounter++.ToString() + "/";
-            // while(Directory.Exists(tempFolderName))
-            //     tempFolderName = folderName + dirCounter++.ToString() + "/";
-            // Directory.CreateDirectory(tempFolderName);
-
-            // File.WriteAllText(tempPath + ".json", skeletonData[frameCounter]);
 
             int dirCounter = 0;
             string tempPath = folderName + dirCounter++.ToString() + ".png";
@@ -221,25 +157,6 @@ namespace UnityEngine
             return skyMat;
         }
 
-        // Material spawnNextFloorMaterial(bool shuffle = true)
-        // {
-        //     Material floorMat;
-
-        //     if(shuffle)
-        //         floorMaterialCounter = Random.Range(0, floorMaterialList.Length);
-        //     else
-        //         floorMaterialCounter++;
-
-        //     if(floorMaterialCounter >= floorMaterialList.Length)
-        //     {
-        //         floorMaterialCounter = 0;
-        //     }
-
-        //     floorMat = (Material)floorMaterialList[floorMaterialCounter];    
-
-        //     return floorMat;
-        // }
-
         Material spawnNextModelMaterial(bool shuffle = true)
         {
             Material modelMat;
@@ -276,15 +193,5 @@ namespace UnityEngine
         {
             screensFolderName = newSaveDir;
         }
-
-        // public bool getAddMirrors()
-        // {
-        //     return addMirrors;
-        // }
-
-        // public void setAddMirrors(bool newAddMirrors)
-        // {
-        //     addMirrors = newAddMirrors;
-        // }
     }
 }
